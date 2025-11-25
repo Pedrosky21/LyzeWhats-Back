c8a6525e18aea27739d7c5e73eaaf535ba21d5b5
Pedro Arreguez <pedroarreguez21@gmail.com>
2025-11-25 20:15:20 -0300

    cors and requirements

--- app/main.py
+++ app/main.py
@@ -1,6 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routers.upload_router import router as upload_router
 
+origins = [
+    "https://lyze-whats.vercel.app"
+]
+
 app = FastAPI(title="LyzeWhats API")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(upload_router, prefix="/api")
